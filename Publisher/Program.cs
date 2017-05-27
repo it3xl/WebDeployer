1198047d466465a688f96f5efe5d08cb952e5292
Ilya Tretyakov <it@it3xl.ru>
2017-05-27 09:56:04 +0300

    Initial commit.

---
+++ Publisher/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class Program
+    {
+        public static readonly string assemblyName = System.Reflection.Assembly
+            .GetExecutingAssembly().GetName().Name;
+
+        private static readonly HttpClient client = new HttpClient
+        ();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var sourcePath = args[0];
+                CheckSourceFile(sourcePath);
+                var targetUrl = args[1];
+                var pulishUrl = new Uri(targetUrl);
+                client.BaseAddress = pulishUrl;
+
+                ProcessPublish(sourcePath)
+                    .Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.Write("<{0}: Exception {1}>", assemblyName, ex.Message);
+            }
+        }
+
+        private async static Task ProcessPublish(string sourcePath)
+        {
+            // https://stackoverflow.com/a/22530157
+            // https://docs.microsoft.com/en-us/aspnet/web-api/overview/advanced/calling-a-web-api-from-a-net-client
+            // https://stackoverflow.com/a/40970671
+
+            HttpResponseMessage response;
+            using (var file = new StreamReader(sourcePath))
+            {
+                var content = new MultipartContent();
+                content.Add(new StreamContent(file.BaseStream));
+                response = await client.PostAsync("api/deployer?fileName=oppaName", content);
+            }
+
+
+
+        }
+
+        private static string CheckSourceFile(string sourcePath)
+        {
+            string absPath;
+            if (Path.IsPathRooted(sourcePath))
+            {
+                absPath = sourcePath;
+            }
+            else
+            {
+                absPath = Path.Combine(Environment.CurrentDirectory, sourcePath);
+            }
+            if (!File.Exists(absPath))
+            {
+                throw new Exception(string.Format("<{0}: There'is no such a file {1}>", assemblyName, absPath));
+            }
+
+            return sourcePath;
+        }
+    }
+}
---
+++ src/DeployerSite/Controllers/DeployerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeployerSite.Controllers
+{
+    [Route("api/[controller]")]
+    public class DeployerController : Controller
+    {
+        // GET: api/deployer
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Service name", "Deployer" };
+        }
+
+        // POST api/deployer
+        [HttpPost]
+        public async Task<JsonResult> Post(string fileName)
+        {
+            // https://stackoverflow.com/a/40970671
+
+
+            try
+            {
+
+                Request.HttpContext.Co
+                var filedata = await Request.ReadFormAsync();
+                    
+                //    .ReadAsMultipartAsync(new MultipartMemoryStreamProvider());
+                foreach (var file in filedata.)
+                {
+                    var fileStream = await file.ReadAsStreamAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            
+            return Json("['oppa']");
+        }
+    }
+}
